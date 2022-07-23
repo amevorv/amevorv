fe5463bfd346241f348b13c80543af67779068c0
BrumNet <brumnetclub@gmail.com>
2022-07-23 06:17:27 -0700

    Commit Mobile1
    Responsiveness for mobile devices Part1

--- js/resp.js
+++ js/resp.js
@@ -1,3 +1,4 @@
+
 var y = window.matchMedia("(max-width: 550px)");
 var x = window.matchMedia("(min-width: 550px) and (max-width: 1000px)");
 myFunctionx(x);myFunctiony(y)// Call listener function at run time
@@ -15,6 +16,8 @@ if(x.matches){
 		nav.className = "fr magb1";
 		heroImage.className = "cb magl1 fl posr w5 z1";
 		heroLine.className = "rot20 w2 colgy";
+		leftStack.className = "w10";
+		rightStack.className = "w10";
   	}
 if(!x.matches){
 		hero.className = "ht";
@@ -25,8 +28,34 @@ if(!x.matches){
 }
 
 function myFunctiony(y) {
-
+	const herox = document.getElementById("hero");
+	const leftStack = document.getElementById("leftstack");
+	const rightStack = document.getElementById("rightstack");
+	const heroDesign = document.getElementById("hero-design");
+	const rightHero = document.getElementById("right-hero");
+	const heroBut = document.getElementById("hero-but");
+	const menu = document.getElementById("menu");
+	const menuIcon = document.getElementById("menu-icon");
 
 if(y.matches){
+		herox.className = "mxht";
+		leftStack.className = "w9";
+		rightStack.className = "w9";
+		heroDesign.className = "dn";
+		rightHero.className = "posr w8 cb fr padr1 h3 z3";
+		rightHero.style.transform ="translate(0, -20px)"
+		menu.className = "dn"
+		menuIcon.className = "dib posr"
+		heroBut.className="but bcolb colw fr magt2 padl2 padr2 lh fs4 magb2"
+		  	}
+
+if(!y.matches){
+		leftStack.className = "w4";
+		rightStack.className = "w4";
+		heroDesign.className = "posr fl tu w8 z2";
+		rightHero.className = "posr w3 fr h3 z3";
+		menu.className = "fs4"
+		menuIcon.className = "dn"
+		heroBut.className = "but bcolb colw fr magt2 padl2 padr2 lh fs6"
 		  	}
 }
